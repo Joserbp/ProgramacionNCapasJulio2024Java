9b311ca13288d838415b2df454fefeb83629acce
JoseRBP <josera.jack@gmail.com>
2024-07-26 11:49:37 -0600

    GetAll

--- src/main/java/com/Digis01/ProgramacionNCapasJulio24/Controller/AlumnoController.java
+++ src/main/java/com/Digis01/ProgramacionNCapasJulio24/Controller/AlumnoController.java
@@ -5,8 +5,11 @@
 package com.Digis01.ProgramacionNCapasJulio24.Controller;
 
 import com.Digis01.ProgramacionNCapasJulio24.DAO.AlumnoDAOImplementation;
+import com.Digis01.ProgramacionNCapasJulio24.ML.Alumno;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -22,8 +25,9 @@ public class AlumnoController {
     private AlumnoDAOImplementation alumnoDAOImplementation;
     
     @GetMapping("/GetAll")
-    public String GetAll(){
+    public String GetAll(Model model){
         Object datos = alumnoDAOImplementation.GetAll();
+        model.addAttribute("Alumnos", (List<Alumno>)datos);
         return "GetAll";
     }
     
