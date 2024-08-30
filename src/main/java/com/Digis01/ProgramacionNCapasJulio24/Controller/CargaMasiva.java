e673b01ac17e0e1e87aa1395e5ffdb0552130512
JoseRBP <josera.jack@gmail.com>
2024-08-30 12:14:56 -0600

    CargaMasiva Errores Completa

--- src/main/java/com/Digis01/ProgramacionNCapasJulio24/Controller/CargaMasiva.java
+++ src/main/java/com/Digis01/ProgramacionNCapasJulio24/Controller/CargaMasiva.java
@@ -7,7 +7,9 @@ package com.Digis01.ProgramacionNCapasJulio24.Controller;
 import com.Digis01.ProgramacionNCapasJulio24.DAO.AlumnoDAOImplementation;
 import com.Digis01.ProgramacionNCapasJulio24.ML.Alumno;
 import com.Digis01.ProgramacionNCapasJulio24.ML.ErrorExcel;
+import com.Digis01.ProgramacionNCapasJulio24.ML.Result;
 import com.Digis01.ProgramacionNCapasJulio24.ML.Semestre;
+import jakarta.servlet.http.HttpSession;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -42,113 +44,136 @@ import org.springframework.web.multipart.MultipartFile;
 @Controller
 @RequestMapping("/CargaMasiva")
 public class CargaMasiva {
-    
+
     @Autowired
     private AlumnoDAOImplementation alumnoDAOImplementation;
-    
+
     @GetMapping("/Get")
-    public String Get(){
+    public String Get() {
+        //model.addAtrribute("errores" null) //NO USAR DE PREFERENCIA
         return "CargaMasivaGet";
-    } 
-    
-    @PostMapping("/txt")
-    public String CargaTxt(@RequestParam MultipartFile txtFile){
-       // String path = "C:\\Users\\digis\\OneDrive\\Documentos\\Jose Ramon Becerra Perez\\Proyectos\\Java\\ProgramacionNCapasJulio24\\CargaMasiva.txt";
-        try{
-        //FileReader fileReader = new FileReader(path);
-        InputStream inputStream = txtFile.getInputStream();
-        InputStreamReader inputReader = new InputStreamReader(inputStream);
-        BufferedReader bufferedReader = new BufferedReader(inputReader);
-        
-        //FileReader   Caracter por caracter h
-        //bufferedReader     Linea por Linea          
-        //Reader - Todo el documento en una sola consulta LIST
-        String linea;
-        //Diego Alejandro|Saavedra|234567|34567|H|2|diego@gmail.com|2002-09-17
-        bufferedReader.readLine();
-           while((linea = bufferedReader.readLine()) != null){
-           String [] datos = linea.split("\\|");
-           Alumno alumno = new Alumno();
-           alumno.setNombre(datos[0]);
-           alumno.setApellido(datos[1]);
-           alumno.setSemestre(new Semestre());
-           alumno.getSemestre().setIdSemestre(Integer.parseInt(datos[5]));
-           
-           alumnoDAOImplementation.AddJPA(alumno);
-         }
-        }
-        catch(IOException ex){
-            
-        }
-        catch(Exception ex){
-        
-        }
-       return "redirect" ;
     }
-    
-    @PostMapping("/excel")
-    public String CargaExcel(Model model,@RequestParam MultipartFile excelFile){
-    try {
-        if(excelFile != null && !excelFile.isEmpty()){
-            
-            String extension = StringUtils.getFilenameExtension(excelFile.getOriginalFilename());
-            if(extension.equals("xlsx")){
-                //Copia del excel //Historico // Validar/Seguridad/Respaldo
-                String root = System.getProperty("user.dir"); //Raiz del proyecto
-                String path = "/src/main/resources/static/ArchivosExcel/";
-                String fecha = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMddHHmmss"));
-                //C:User......../src/res/sta/archEx/20240827170112CargaMasiva.xlsx
-                String nombreArchivo = excelFile.getOriginalFilename(); //Extension
-                String pathFinal = root + path + fecha + nombreArchivo; 
-            
-                excelFile.transferTo(new File(pathFinal)); //Guarda un archivo copia
-               
-                List<Alumno> alumnos = LeerExcel(pathFinal);
-                List<ErrorExcel> errores = ValidarDatos(alumnos);  // Mostrar errores en caso de existir
-                if(errores.size() > 0){ //Hay Errores
-                    model.addAttribute("ListaErrores", errores);
-                    return "CargaMasivaGet";
-                }
-                
-            }else{
-                //Mensaje de error
+
+    @PostMapping("/txt")
+    public String CargaTxt(@RequestParam MultipartFile txtFile) {
+        // String path = "C:\\Users\\digis\\OneDrive\\Documentos\\Jose Ramon Becerra Perez\\Proyectos\\Java\\ProgramacionNCapasJulio24\\CargaMasiva.txt";
+        try {
+            //FileReader fileReader = new FileReader(path);
+            InputStream inputStream = txtFile.getInputStream();
+            InputStreamReader inputReader = new InputStreamReader(inputStream);
+            BufferedReader bufferedReader = new BufferedReader(inputReader);
+
+            //FileReader   Caracter por caracter h
+            //bufferedReader     Linea por Linea          
+            //Reader - Todo el documento en una sola consulta LIST
+            String linea;
+            //Diego Alejandro|Saavedra|234567|34567|H|2|diego@gmail.com|2002-09-17
+            bufferedReader.readLine();
+            while ((linea = bufferedReader.readLine()) != null) {
+                String[] datos = linea.split("\\|");
+                Alumno alumno = new Alumno();
+                alumno.setNombre(datos[0]);
+                alumno.setApellido(datos[1]);
+                alumno.setSemestre(new Semestre());
+                alumno.getSemestre().setIdSemestre(Integer.parseInt(datos[5]));
+
+                alumnoDAOImplementation.AddJPA(alumno);
             }
-        }else{
-            //Mesaje de error
+        } catch (IOException ex) {
+
+        } catch (Exception ex) {
+
         }
-    } catch (IOException ex) {
-                    Logger.getLogger(CargaMasiva.class.getName()).log(Level.SEVERE, null, ex);
-                } catch (IllegalStateException ex) {
-                    Logger.getLogger(CargaMasiva.class.getName()).log(Level.SEVERE, null, ex);
+        return "redirect";
+    }
+
+    @PostMapping("/excel")
+    public String CargaExcel(Model model, @RequestParam MultipartFile excelFile, HttpSession session) {
+        try {
+            if (excelFile != null && !excelFile.isEmpty()) {
+
+                String extension = StringUtils.getFilenameExtension(excelFile.getOriginalFilename());
+                if (extension.equals("xlsx")) {
+                    //Copia del excel //Historico // Validar/Seguridad/Respaldo
+                    String root = System.getProperty("user.dir"); //Raiz del proyecto
+                    String path = "/src/main/resources/static/ArchivosExcel/";
+                    String fecha = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMddHHmmss"));
+                    //C:User......../src/res/sta/archEx/20240827170112CargaMasiva.xlsx
+                    String nombreArchivo = excelFile.getOriginalFilename(); //Extension
+                    String pathFinal = root + path + fecha + nombreArchivo;
+
+                    excelFile.transferTo(new File(pathFinal)); //Guarda un archivo copia
+
+                    List<Alumno> alumnos = LeerExcel(pathFinal);
+                    List<ErrorExcel> errores = ValidarDatos(alumnos);  // Mostrar errores en caso de existir
+                    if (errores.size() > 0) { //Hay Errores
+                        model.addAttribute("ListaErrores", errores);
+                        return "CargaMasivaGet";
+                    } else {
+                        model.addAttribute("Mensaje", "El archivo es correcto, esta listo para subir");
+                        //PATHFINAL  --> Almacenar en una session
+                        session.setAttribute("path", pathFinal);
+                        return "CargaMasivaGet";
+                    }
+
+                } else {
+                    //Mensaje de error
                 }
+            } else {
+                //Mesaje de error
+            }
+        } catch (IOException ex) {
+            Logger.getLogger(CargaMasiva.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IllegalStateException ex) {
+            Logger.getLogger(CargaMasiva.class.getName()).log(Level.SEVERE, null, ex);
+        }
         //isExcel
         //isEmpty
         //Validar Informacion
         //Validar Campos
         //Tipos de datos en los campos
-        
-        return "redirect" ;
+
+        return "redirect";
     }
-    
-    
-    public List<Alumno> LeerExcel(String pathExcel){
+
+    @PostMapping("/excel/Subir")
+    public String SubirExcel(HttpSession session) {
+        String pathFinal = session.getAttribute("path").toString();
+        try {
+            List<Alumno> alumnos = LeerExcel(pathFinal);
+            for (Alumno alumno : alumnos) {
+                Result result =alumnoDAOImplementation.AddJPA(alumno);
+                if (!result.correct) {
+                    //Generar una lista de Errores
+                    //Generar un TXT - Log
+                    //Descarga automatica
+                }
+            }
+        } catch (Exception ex) {
+           //Manejo de la ex
+        }
+        //session.setAttribute("path", null);
+        session.removeAttribute("path");
+        return "redirect";
+    }
+
+    public List<Alumno> LeerExcel(String pathExcel) {
         List<Alumno> alumnos = new ArrayList<>();
-        try{
-        XSSFWorkbook workBook = new XSSFWorkbook(pathExcel);
-        Sheet sheet = workBook.getSheetAt(0);
-        
-        boolean isPrimeraFila = true;
-        for(Row row : sheet){  //Lectura es de inicio a fin
-           // if(row[0] ) continue;
-            //if(row.getCell(0).toString().equals("Nombre")) continue;
-            if(isPrimeraFila){
-                isPrimeraFila = false;
-            }else{
-            Alumno alumno = new Alumno();
-            alumno.setNombre(row.getCell(0).toString());
-            alumno.setApellido(row.getCell(1).toString());
-            alumno.setSemestre(new Semestre());
-            //Sin apache
+        try ( XSSFWorkbook workBook = new XSSFWorkbook(pathExcel)) {
+            Sheet sheet = workBook.getSheetAt(0);
+
+            boolean isPrimeraFila = true;
+            for (Row row : sheet) {  //Lectura es de inicio a fin
+                // if(row[0] ) continue;
+                //if(row.getCell(0).toString().equals("Nombre")) continue;
+                if (isPrimeraFila) {
+                    isPrimeraFila = false;
+                } else {
+                    Alumno alumno = new Alumno();
+                    alumno.setNombre(row.getCell(0).toString());
+                    alumno.setApellido(row.getCell(1).toString());
+                    alumno.setSemestre(new Semestre());
+                    //Sin apache
 //            if(!row.getCell(5).toString().equals("")){
 //            String idSemestre = row.getCell(5).toString();
 //            double numeroDouble = Double.parseDouble(idSemestre);
@@ -156,19 +181,18 @@ public class CargaMasiva {
 //            alumno.getSemestre().setIdSemestre(numeroInt);
 //            }
 //          //Con libreria de Apache
-            String idSemestre = row.getCell(5).toString();
-            double idSemestreDouble = NumberUtils.toDouble(idSemestre, 0); //Ex(X) default
-            int idSemestreInt = (int)idSemestreDouble;
-            alumno.getSemestre().setIdSemestre(idSemestreInt);
-            alumnos.add(alumno);
-            }  
-        }
-        }
-            catch(Exception ex){
-            
+                    String idSemestre = row.getCell(5).toString();
+                    double idSemestreDouble = NumberUtils.toDouble(idSemestre, 0); //Ex(X) default
+                    int idSemestreInt = (int) idSemestreDouble;
+                    alumno.getSemestre().setIdSemestre(idSemestreInt);
+                    alumnos.add(alumno);
+                }
+            }
+        } catch (Exception ex) {
+
         }
         return alumnos;
-        
+
         //Filas 4
         //Columnas 4
         //Celdas  3 //DATOS
@@ -179,19 +203,19 @@ public class CargaMasiva {
     public List<ErrorExcel> ValidarDatos(List<Alumno> alumnos) {
         List<ErrorExcel> erroresExcel = new ArrayList<ErrorExcel>();
         int fila = 2;
-        for(Alumno alumnoAValidar : alumnos){
+        for (Alumno alumnoAValidar : alumnos) {
             String errores = "";
-            if(alumnoAValidar.getNombre().equals("")){ //STRING
-                errores += "Falta el nombre, " ;
+            if (alumnoAValidar.getNombre().equals("")) { //STRING
+                errores += "Falta el nombre, ";
             }
             //(condicion) ? true : false
             errores += (alumnoAValidar.getApellido().equals("")) ? "Falta Apellido, " : "";
-            
-            if(alumnoAValidar.getSemestre().getIdSemestre() == 0){
-                 errores += "Falta el IdSemestre, " ;
+
+            if (alumnoAValidar.getSemestre().getIdSemestre() == 0) {
+                errores += "Falta el IdSemestre, ";
             }
-            
-            if(!errores.equals("")){
+
+            if (!errores.equals("")) {
                 ErrorExcel errorExcel = new ErrorExcel(fila, errores);
                 erroresExcel.add(errorExcel);
             }
