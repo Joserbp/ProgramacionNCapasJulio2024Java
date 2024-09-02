e53cc7f76c692a01c85b92fbcc08d130f04efe52
JoseRBP <josera.jack@gmail.com>
2024-09-02 16:55:40 -0600

    REST RepsonseEntity

--- src/main/java/com/Digis01/ProgramacionNCapasJulio24/RestController/OperacionController.java
+++ src/main/java/com/Digis01/ProgramacionNCapasJulio24/RestController/OperacionController.java
@@ -4,6 +4,11 @@
  */
 package com.Digis01.ProgramacionNCapasJulio24.RestController;
 
+import java.net.http.HttpResponse;
+import java.util.HashMap;
+import java.util.Map;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,8 +25,29 @@ public class OperacionController {
     
     
     @GetMapping("/Saludar/{Nombre}") //ENDPOINT
-    public String Saludar(@PathVariable("Nombre") String Nombre){
-        return "Hola " + Nombre;
-        //HASHMAP  MAP
+    public Map<String,Object> Saludar(@PathVariable("Nombre") String Nombre){
+        //HASHMAP --- (Array) Estructurado Mapeo de llave valor
+        //JSON javaScript object notation
+        Map<String,Object> response = new HashMap<>();
+        response.put("Mensaje", "Hola " + Nombre);
+        response.put("Nombre" , Nombre);
+        return response;
+    }
+    
+    @GetMapping("/Saludar2/{Nombre}") //ENDPOINT
+    public ResponseEntity Saludar2(@PathVariable("Nombre") String Nombre){
+        Map<String,Object> response = new HashMap<>();
+        
+        //ResponseEntity -- Informacion + StatusCode
+        if(Nombre.equals(" ")){
+            response.put("Mensaje", "El nombre esta vacio");
+        return new ResponseEntity(response, HttpStatus.BAD_REQUEST);
+        }else{
+          response.put("Mensaje", "Hola " + Nombre);
+         response.put("Nombre" , Nombre);
+        //ResponseEntity -- Informacion + StatusCode
+        return new ResponseEntity(response, HttpStatus.OK);
+        }
+
     }
 }
